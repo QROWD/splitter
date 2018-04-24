2fd40a148480c3ab1a86f73041334270e8ae746d
lpfgarcia <lpfgarcia@gmail.com>
2018-04-24 13:02:12 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,64 @@
+import sys, os
+import argparse
+import pyarrow.parquet as pq
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(
+    description='Split the accelerometer and GPS  parquet data based on \
+    the answering label'
+  )
+
+  parser.add_argument('--path', help='directory of the parquet file')
+
+  parser.add_argument('--label', help='label of the transportation mode:' + 
+    '{Car, Auto, Bike}')
+
+  path = os.path.dirname(os.path.realpath(os.path.basename(__file__)))
+  args = parser.parse_args()
+
+  print(args)
+
+  data = pq.ParquetDataset(str(args.path) + "/questionnaireanswers.parquet/")
+  data = data.read().to_pandas()
+  data = data.fillna(0)
+
+  aux = data.loc[data["answerstringb"] == str(args.label), "questiontimestamp"]
+
+  print(aux)
+  aux = aux.index.values
+
+  A = data.iloc[aux-1, 1]
+  B = data.iloc[aux, 1]
+
+  data = pq.ParquetDataset(str(args.path) + "accelerometerevent.parquet/")
+  data = data.read().to_pandas()
+  data = data.fillna(0)
+
+  base = []
+  for a, b in zip(A, B):
+    mask = (data['timestamp'] > a) & (data['timestamp'] <= b)
+    base.append(data.loc[mask])
+
+  base = pd.concat(base)
+  base = base.drop(['day'], axis=1)
+
+  pd.DataFrame.to_csv(base, "/home/lpfgarcia/Desktop/" + str(args.label) + 
+    "_acc_bruto.csv")
+
+  data = pq.ParquetDataset(str(args.path) + "locationeventpertime.parquet/")
+  data = data.read().to_pandas()
+  data = data.fillna(0)
+
+  base = []
+  for a, b in zip(A, B):
+    mask = (data['timestamp'] > a) & (data['timestamp'] <= b)
+    base.append(data.loc[mask])
+
+  base = pd.concat(base)
+  base = base.drop(['day'], axis=1)
+
+  pd.DataFrame.to_csv(base, "/home/lpfgarcia/Desktop/" + str(args.label) + 
+    "_gps_bruto.csv")
